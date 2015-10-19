158c79605751f4d7cd8e0ca77c21b67438690e98
nortoncg <nortoncg@hotmail.com>
2015-10-19 18:47:58 -0200

    Inclusão do frmCadPessoa, Pessoa e pPessoas.

---
+++ Persistencia/pPessoas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    class Pessoas
+    {
+        String cpf;
+        String nome;
+        String dataNascimento;
+
+        public void inserir()
+        {
+
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+
+        public String buscar_CPF()
+        {
+            return "";
+        }
+
+        public int calcularIdade()
+        {
+            return 1;
+        }
+    }
+}
